1466090069b95ad618a2a450900a3a3a0b086997
Soham Panjwani <126118065+S-Panjwani@users.noreply.github.com>
2025-01-04 19:17:38 -0700

    PHP

---
+++ 30 LEVEL MODULES/CSE 3330 DYNAMIC DATASTRUCTURES 2/QUEUES.php
@@ -0,0 +1,12 @@
+<?php
+$person = array(
+    "name" => "John",
+    "age" => 30,
+    "city" => "New York"
+);
+
+// Accessing values using keys
+echo $person["name"]; // Outputs: John
+echo $person["age"];  // Outputs: 30
+
+?>
